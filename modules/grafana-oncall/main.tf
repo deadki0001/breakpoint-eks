7445bb665573240a4c9d8b83467eccce42f69f6e
Devon Adkins <adkins.devon@hotmail.com>
2026-08-27 12:26:13 +0200

    formatting terraform code for grafana oncall

--- modules/grafana-oncall/main.tf
+++ modules/grafana-oncall/main.tf
@@ -43,6 +43,6 @@ data "grafana_oncall_user" "on_call_engineer" {
 resource "grafana_oncall_escalation" "notify_user" {
   escalation_chain_id = grafana_oncall_escalation_chain.this.id
   type                = "notify_persons"
-  persons_to_notify    = [data.grafana_oncall_user.on_call_engineer.id]
-  position             = 0
+  persons_to_notify   = [data.grafana_oncall_user.on_call_engineer.id]
+  position            = 0
 }
